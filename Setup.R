eec9a1576450c00543c4dab502edd2a2d5892772
zain-butt <89316101+zain-butt@users.noreply.github.com>
2021-08-21 16:13:42 -0400

    Install required libraries
    Rstudio

---
+++ Setup.R
@@ -0,0 +1,21 @@
+#######################################
+############## LIBRARIES ##############
+#######################################
+
+library(BatchGetSymbols)
+library(rvest)
+library(dplyr)
+library(ggplot2)
+library(quantmod)
+library(tidyverse)
+library(cowplot)
+library(stargazer)
+library(dplyr)
+library(formattable)
+library(tseries)
+library(DT)
+library(shiny)
+library(webshot)
+library(blastula)
+library(keyring)
+library(mailR)
